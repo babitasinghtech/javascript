67e7194ac3aaad7ec5d58fe3df770f629e60d7f8
Babita Singh <babitasinghtech@gmail.com>
2025-04-05 20:17:18 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,71 @@
+// console.log("ram ram ji")
+// function exam() {
+
+// var x = 12;
+// console.log(x);
+// }
+// exam();
+// console.log(x);
+
+// function example() {
+//     var x = 10;
+//     console.log(x);
+// }
+
+// // example();       // 10
+ 
+// // ```
+// let x = 10;
+// let y = 20;
+// if (true) {
+//     let y = 20;
+//     console.log(x); // Output: 10
+//     console.log(y); // Output: 20
+// }
+// console.log(x); // Output: 10
+// console.log(y); // Throws ReferenceError: y is not defined
+
+
+// let z = 10;
+// console.log(z);
+
+// z = "rama";
+// console.log(z);
+
+// //let g= 10;
+// let g = "shyama";
+// console.log(g);
+
+
+// // ```
+// const f = 20;
+// console.log(f);      // Output : 20
+
+// h = 30;
+// console.log(h);      // Output : Error!
+// // //```
+// // //let l;      // No Error
+
+// // var k;      // No Error
+
+// // const m;    // Error! "Missing initializer in const declaration"
+
+// const person ={
+//     name : "Radha",
+//     age: 24
+
+// };
+// console.log(person.name);
+// person.name = "love";
+// console.log(person.name);
+// //person={};
+
+// // ```
+// const numbers = [1, 2, 3];
+// console.log(numbers);    // Output: [1, 2, 3]
+
+// // Modifying array elements is allowed
+// numbers.push(4);
+// console.log(numbers);    // Output: [1, 2, 3, 4]
+
+
